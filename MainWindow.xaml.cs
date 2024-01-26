d4023c564e94e32b6165e32ff6d373d1ba529c58
eric-dybsetter <eric.dybsetter@gmail.com>
2024-01-26 15:59:34 -0500

    Can play a wav file

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System;
+using System.IO;
+using Microsoft.Win32;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Somnisonus
 {
@@ -16,9 +22,53 @@ namespace Somnisonus
     /// </summary>
     public partial class MainWindow : Window
     {
+        private WaveOutEvent outputDevice;
+        private AudioFileReader audioFile;
+        private string filename;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnOpenFiles_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = false;
+            openFileDialog.Filter = "WAV files (*.wav)|*.wav|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            if (openFileDialog.ShowDialog() == true) {
+                filename = openFileDialog.FileName;                
+            }
+        }
+
+        private void OnPlaybackStopped(object sender, StoppedEventArgs args)
+        {
+            outputDevice.Dispose();
+            outputDevice = null;
+            audioFile.Dispose();
+            audioFile = null;
+        }
+
+        private void play_Click(object sender, RoutedEventArgs e)
+        {
+            if (outputDevice == null)
+            {
+                outputDevice = new WaveOutEvent();
+                outputDevice.PlaybackStopped += OnPlaybackStopped;
+            }
+            if (audioFile == null)
+            {
+                audioFile = new AudioFileReader(filename);
+                outputDevice.Init(audioFile);
+            }
+            nowPlaying.Text.Concat(filename);
+            outputDevice.Play();
+        }
+
+        private void stop_Click(object sender, RoutedEventArgs e)
+        {
+            outputDevice?.Stop();
         }
     }
 }
