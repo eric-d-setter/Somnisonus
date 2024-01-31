3b13162d953a5fdfc4faeaa9ce15d5e17726920a
eric-dybsetter <eric.dybsetter@gmail.com>
2024-01-31 14:59:13 -0500

    Saving work

---
+++ AudioCollection.cs
@@ -0,0 +1,36 @@
+﻿using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace Somnisonus
+{
+    public class AudioCollection
+    {
+        private ConcatenatingSampleProvider concatenatingSampleProvider;
+        private bool currentlyLooping;
+        public AudioCollection(List<CachedSound> sounds, bool loop) {
+            List<CachedSoundSampleProvider> sampleProviders = new List<CachedSoundSampleProvider>();
+            foreach (var sound in sounds)
+            {
+                sampleProviders.Add(new CachedSoundSampleProvider(sound));
+            }
+            concatenatingSampleProvider = new ConcatenatingSampleProvider(sampleProviders);
+            currentlyLooping = loop;
+        }
+
+        public void DeactivateLoop()
+        {
+            currentlyLooping = false;
+        }
+
+        public bool GetLoopingStatus()
+        {
+            return currentlyLooping;
+        }
+
+        public ConcatenatingSampleProvider GetConcatenatingSampleProvider()
+        {
+            return concatenatingSampleProvider;
+        }
+        
+    }
+}
