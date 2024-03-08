f57bc10ab6aa2721657fff0c5110ef7aae6e01ca
eric-dybsetter <eric.dybsetter@gmail.com>
2024-03-08 13:55:27 -0500

    Starting processing for JSON parsing

--- AudioCollectionParser.cs
+++ AudioCollectionParser.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using System.Text.Json;
 
 namespace Somnisonus
 {
     internal class AudioCollectionParser
     {
+        private readonly string _sampleJsonFilePath;
+        public AudioCollectionParser(string sampleJsonFilePath)
+        {
+            _sampleJsonFilePath = sampleJsonFilePath;
+        }
+
+        private readonly JsonSerializerOptions _options = new()
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        public ParsedAudioCollection UseFileOpenReadTextWithSystemTextJson()
+        {
+            using FileStream json = File.OpenRead(_sampleJsonFilePath);
+            List<ParsedAudioCollection> collection = JsonSerializer.Deserialize<List<ParsedAudioCollection>>(json, _options);
+            if (collection != null) 
+            {
+                return collection.First(); //One collection per JSON file
+            }
+            else
+            {
+                return null;
+            }
+           
+        }
     }
 }
---
+++ ParsedAudioCollection.cs
@@ -0,0 +1,19 @@
+﻿namespace Somnisonus
+{
+    public class ParsedAudioCollection
+    {
+        public String Collection_name { get; set; }
+        public List<ParsedAudioSegment> Collection_data { get; set; }
+    }
+    public class ParsedAudioSegment 
+    {
+        public String Type { get; set; }
+        public List<ParsedAudioSounds> Sounds { get; set; }
+    }
+    public class ParsedAudioSounds
+    {
+        public String Path { get; set; }
+        public int Cutoff_start { get; set; }
+        public int Cutoff_end { get; set; }
+    }
+}
