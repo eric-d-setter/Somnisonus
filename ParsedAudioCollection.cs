221f85c975042f6e7a99f72083df421f8189c685
eric-dybsetter <eric.dybsetter@gmail.com>
2024-04-02 11:14:48 -0400

    Cleaning up some info

--- ParsedAudioCollection.cs
+++ ParsedAudioCollection.cs
@@ -3,13 +3,13 @@
     public class ParsedAudioCollection
     {
         public String Collection_name { get; set; }
-        public List<ParsedAudioSegment> Collection_data { get; set; }
+        public List<ParsedAudioSegment> Collection_data { get; set; } = new List<ParsedAudioSegment>();
     }
     public class ParsedAudioSegment 
     {
         public String Type { get; set; }
         public int Order { get; set; }
-        public List<ParsedAudioSounds> Sounds { get; set; }
+        public List<ParsedAudioSounds> Sounds { get; set; } = new List<ParsedAudioSounds>();
     }
     public class ParsedAudioSounds
     {
--- ParsedAudioRoadmap.cs
+++ ParsedAudioRoadmap.cs
@@ -3,13 +3,13 @@
     public class ParsedAudioRoadmap
     {
         public String Roadmap_name { get; set; }
-        public List<ParsedAudioData> Collection_data { get; set; }
+        public List<ParsedAudioData> Collection_data { get; set; } = new List<ParsedAudioData>();
         public String Start {  get; set; }
     }
     public class ParsedAudioData
     {
         public String Collection_name { get; set; }
-        public List<ParsedNextData>? Next { get; set; } = null;
+        public List<ParsedNextData> Next { get; set; } = new List<ParsedNextData>();
     }
     public class ParsedNextData
     {
